fa83d21d75a8efd1a5f12dd946848b0c0eee5f0b
davidcharlebois6 <david_charlebois@msn.com>
2015-10-08 22:16:25 -0400

    Busway large template

---
+++ Transit.Addon.RoadExtensions/Roads/Busway6L/Busway6LBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Transit.Addon.RoadExtensions.Menus;
+using Transit.Framework;
+using Transit.Framework.Modularity;
+
+namespace Transit.Addon.RoadExtensions.Roads.Busway6L
+{
+    public class Busway6LBuilder : NetInfoBuilderBase, INetInfoBuilder
+    {
+        public int Order { get { return 170; } }
+        public int Priority { get { return 26; } }
+
+        public string TemplatePrefabName { get { return NetInfos.Vanilla.ROAD_6L; } }
+        public string Name { get { return "Large Busway"; } }
+        public string DisplayName { get { return "Busway"; } }
+        public string CodeName { get { return "BUSWAY_6L"; } }
+        public string Description { get { return "A two-lane, two-way road suitable for buses only. Busway does not allow zoning next to it!"; } }
+        public string UICategory { get { return AdditionnalMenus.ROADS_BUSWAYS; } }
+
+        public string ThumbnailsPath { get { return null; } }
+        public string InfoTooltipPath { get { return null; } }
+
+        public NetInfoVersion SupportedVersions
+        {
+            get { return NetInfoVersion.All; }
+        }
+
+        public void BuildUp(NetInfo info, NetInfoVersion version)
+        {
+        }
+    }
+}
