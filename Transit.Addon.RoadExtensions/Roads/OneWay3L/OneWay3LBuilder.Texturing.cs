1d6e6d5da3b0c1b233a4b4b42328fbf51269fc7c
andreharv <andreharv@yahoo.com>
2015-11-02 07:38:23 -0500

    Elevated 3L road first draft

---
+++ Transit.Addon.RoadExtensions/Roads/OneWay3L/OneWay3LBuilder.Texturing.cs
@@ -0,0 +1,44 @@
+﻿using Transit.Framework;
+
+namespace Transit.Addon.RoadExtensions.Roads.OneWay3L
+{
+    public partial class OneWay3LBuilder
+    {
+        private static void SetupTextures(NetInfo info, NetInfoVersion version)
+        {
+            switch (version)
+            {
+                case NetInfoVersion.Ground:
+                    info.SetAllSegmentsTexture(
+                        new TexturesSet
+                            (@"Roads\OneWay3L\Textures\Ground\Segment__MainTex.png",
+                            @"Roads\OneWay3L\Textures\Ground\Segment__AlphaMap.png"),
+                        new LODTexturesSet
+                            (@"Roads\OneWay3L\Textures\Ground\SegmentLOD__MainTex.png",
+                            @"Roads\OneWay3L\Textures\Ground\SegmentLOD__AlphaMap.png",
+                            @"Roads\OneWay3L\Textures\Ground\SegmentLOD__XYS.png"));
+                    break;
+                case NetInfoVersion.Elevated:
+                case NetInfoVersion.Bridge:
+                    info.SetAllSegmentsTexture(
+                        new TexturesSet
+                            (@"Roads\OneWay3L\Textures\ElevatedBridge\MainTex.png",
+                            @"Roads\OneWay3L\Textures\ElevatedBridge\Segment__APRMap.png"),
+						new LODTexturesSet
+							(@"Roads\OneWay3L\Textures\ElevatedBridge\LOD__MainTex.png",
+							@"Roads\OneWay3L\Textures\ElevatedBridge\SegmentLOD__APRMap.png",
+							@"Roads\OneWay3L\Textures\ElevatedBridge\LOD__XYSMap.png"));
+					info.SetAllNodesTexture(
+                        new TexturesSet
+                            (@"Roads\OneWay3L\Textures\ElevatedBridge\MainTex.png",
+                            @"Roads\OneWay3L\Textures\ElevatedBridge\Node__APRMap.png"),
+                        new LODTexturesSet
+                            (@"Roads\OneWay3L\Textures\ElevatedBridge\LOD__MainTex.png",
+                            @"Roads\OneWay3L\Textures\ElevatedBridge\NodeLOD__APRMap.png",
+                            @"Roads\OneWay3L\Textures\ElevatedBridge\LOD__XYSMap.png"));
+                    break;
+            }
+        }
+    }
+}
+
--- Transit.Addon.RoadExtensions/Roads/OneWay3L/OneWay3LBuilder.cs
+++ Transit.Addon.RoadExtensions/Roads/OneWay3L/OneWay3LBuilder.cs
@@ -9,7 +9,7 @@ using Transit.Addon.RoadExtensions.Menus;
 
 namespace Transit.Addon.RoadExtensions.Roads.OneWay3L
 {
-    public class OneWay3LBuilder : Activable, INetInfoBuilderPart
+    public partial class OneWay3LBuilder : Activable, INetInfoBuilderPart
     {
         public int Order { get { return 8; } }
         public int UIOrder { get { return 10; } }
@@ -20,12 +20,12 @@ namespace Transit.Addon.RoadExtensions.Roads.OneWay3L
         public string Description { get { return "A three-lane one-way road without parkings spaces. Supports medium traffic."; } }
         public string UICategory { get { return AdditionnalMenus.ROADS_SMALL_HV; } }
 
-        public string ThumbnailsPath    { get { return @"Roads\OneWay3L\thumbnails.png"; } }
-        public string InfoTooltipPath   { get { return @"Roads\OneWay3L\infotooltip.png"; } }
+        public string ThumbnailsPath { get { return @"Roads\OneWay3L\thumbnails.png"; } }
+        public string InfoTooltipPath { get { return @"Roads\OneWay3L\infotooltip.png"; } }
 
         public NetInfoVersion SupportedVersions
         {
-            get { return NetInfoVersion.Ground; }
+            get { return NetInfoVersion.All; }
         }
 
         public void BuildUp(NetInfo info, NetInfoVersion version)
@@ -38,51 +38,20 @@ namespace Transit.Addon.RoadExtensions.Roads.OneWay3L
             ///////////////////////////
             // 3DModeling            //
             ///////////////////////////
-            if (version == NetInfoVersion.Ground)
-            {
-                var segments0 = info.m_segments[0];
-                var nodes0 = info.m_nodes[0];
-
-                segments0.m_forwardRequired = NetSegment.Flags.None;
-                segments0.m_forwardForbidden = NetSegment.Flags.None;
-                segments0.m_backwardRequired = NetSegment.Flags.None;
-                segments0.m_backwardForbidden = NetSegment.Flags.None;
-                segments0.SetMeshes
-                    (@"Roads\SmallHeavyRoads\Meshes\Ground.obj",
-                     @"Roads\SmallHeavyRoads\Meshes\Ground_LOD.obj");
-
-                nodes0.SetMeshes
-                    (@"Roads\SmallHeavyRoads\Meshes\Ground.obj",
-                     @"Roads\SmallHeavyRoads\Meshes\Ground_Node_LOD.obj");
-
-                info.m_segments = new[] { segments0 };
-                info.m_nodes = new[] { nodes0 };
-            }
+            SetupModels(info, version);
 
             ///////////////////////////
             // Texturing             //
             ///////////////////////////
-            switch (version)
-            {
-                case NetInfoVersion.Ground:
-                    info.SetAllSegmentsTexture(
-                        new TexturesSet
-                           (@"Roads\OneWay3L\Textures\Ground_Segment__MainTex.png",
-                            @"Roads\OneWay3L\Textures\Ground_Segment__AlphaMap.png"),
-                        new LODTexturesSet
-                           (@"Roads\OneWay3L\Textures\Ground_SegmentLOD__MainTex.png",
-                            @"Roads\OneWay3L\Textures\Ground_SegmentLOD__AlphaMap.png",
-                            @"Roads\OneWay3L\Textures\Ground_SegmentLOD__XYS.png"));
-                    break;
-            }
-
+            SetupTextures(info, version);
 
             ///////////////////////////
             // Set up                //
             ///////////////////////////
             info.m_hasParkingSpaces = false;
             info.m_class = owRoadInfo.m_class.Clone(NetInfoClasses.NEXT_SMALL3L_ROAD);
-            info.m_pavementWidth = 2;
+            info.m_pavementWidth = 3;
+            info.m_halfWidth = 8;
             info.m_class.m_level = ItemClass.Level.Level3; // To make sure they dont fit with the 4L Small Roads
 
             // Setting up lanes
@@ -101,9 +70,9 @@ namespace Transit.Addon.RoadExtensions.Roads.OneWay3L
                 .First();
 
             var vehicleLanes = new List<NetInfo.Lane>();
-            const float outerCarLanePosition = 4.0f;
-            const float pedLanePosition = 8f;
-            const float pedLaneWidth = 1.5f;
+            const float outerCarLanePosition = 3.0f;
+            const float pedLanePosition = 5.5f;
+            const float pedLaneWidth = 2.5f;
 
             for (int i = 0; i < 3; i++)
             {
@@ -154,6 +123,18 @@ namespace Transit.Addon.RoadExtensions.Roads.OneWay3L
                 .OrderBy(l => l.m_position)
                 .ToArray();
 
+            //Setting Up Props
+
+            var propLanes = info.m_lanes.Where(l => l.m_laneProps != null && (l.m_laneProps.name.ToLower().Contains("left") || l.m_laneProps.name.ToLower().Contains("right"))).ToList();
+
+            if (propLanes.Count > 0)
+            {
+                for (int i = 0; i < propLanes.Count(); i++)
+                {
+                    var positionMultiplier = Math.Abs(propLanes[i].m_position) / propLanes[i].m_position;
+                    propLanes[i].m_position += positionMultiplier;
+                }
+            }
 
             if (version == NetInfoVersion.Ground)
             {
