bf41773527835125251fe715823eb9414799a2de
davidcharlebois6 <david_charlebois@msn.com>
2015-11-29 18:21:46 -0500

    NetworkExtensions.Deploy

---
+++ NetworkExtensions.Deploy/Mod.Modules.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Transit.Framework;
+using Transit.Framework.Modularity;
+using UnityEngine;
+
+#if DEBUG
+using Debug = Transit.Framework.Debug;
+#endif
+
+namespace NetworkExtensions
+{
+    public partial class Mod
+    {
+        private IEnumerable<IModule> _modules;
+        public IEnumerable<IModule> Modules
+        {
+            get
+            {
+                if (_modules == null)
+                {
+                    try
+                    {
+                        var assetPath = this.GetAssetPath();
+                        var moduleType = typeof(IModule);
+
+                        _modules = AppDomain.CurrentDomain.GetAssemblies()
+                            .SelectMany(a => a.GetTypes())
+                            .Where(t => !t.IsAbstract && !t.IsInterface)
+                            .Where(t => moduleType.IsAssignableFrom(t))
+                            .Where(t => t.GetCustomAttributes(typeof(ModuleAttribute), true)
+                                         .OfType<ModuleAttribute>()
+                                         .Any(a => a.IsAssociatedWith(typeof(Mod))))
+                            .Select(t =>
+                                {
+                                    try
+                                    {
+                                        var module = (IModule)Activator.CreateInstance(t);
+                                        Debug.Log(string.Format("TAM: Loading module {0}", module.Name));
+
+                                        module.AssetPath = assetPath;
+                                        module.SaveSettingsNeeded += ModuleSettingsNeedSave;
+                                        return module;
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Debug.Log("TAM: Crashed-Module " + t.Name);
+                                        Debug.Log("TAM: " + ex.Message);
+                                        Debug.Log("TAM: " + ex.ToString());
+                                        return null;
+                                    }
+                                })
+                            .Where(t => t != null)
+                            .OrderBy(m => m.Order)
+                            .ToArray();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Log("TAM: Crashed-Modules");
+                        Debug.Log("TAM: " + ex.Message);
+                        Debug.Log("TAM: " + ex.ToString());
+
+                        _modules = new IModule[] {};
+                    }
+                }
+
+                return _modules;
+            }
+        }
+    }
+}
---
+++ NetworkExtensions.Deploy/Mod.cs
@@ -0,0 +1,30 @@
+﻿using Transit.Framework.Modularity;
+
+namespace NetworkExtensions
+{
+    public sealed partial class Mod : ITransitMod
+    {
+        public string Name
+        {
+            get
+            {
+                OnGameLoaded();
+                return NAME;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                OnGameLoaded();
+                return DESCRIPTION;
+            }
+        }
+
+        public ulong WorkshopId
+        {
+            get { return 478820060; }
+        }
+    }
+}
