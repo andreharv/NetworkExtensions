6a46c42c3d56927c98d2639a4746e8ba07e4ed58
davidcharlebois6 <david_charlebois@msn.com>
2015-12-05 12:38:51 -0500

    TAM and NExt cohexistance support

--- Transit.Framework/_Extensions/PluginManagerExtensions.cs
+++ Transit.Framework/_Extensions/PluginManagerExtensions.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ColossalFramework.Plugins;
+using UnityEngine;
 
 namespace Transit.Framework
 {
     public static class PluginManagerExtensions
     {
+        public static bool IsPluginInstalled(this PluginManager pluginManager, string pluginId)
+        {
+            foreach (var plugin in pluginManager.GetPluginsInfo())
+            {
+                Debug.Log(string.Format("RARARA: plugin name {0}", plugin.name));
+                if (plugin.name.Equals(pluginId))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         public static bool IsPluginActive(this PluginManager pluginManager, string pluginId)
         {
             foreach (var plugin in pluginManager.GetPluginsInfo())
