1e0d14e6158473887ccd1efd14a9c05f8d4b34b7
davidcharlebois6 <david_charlebois@msn.com>
2015-12-16 23:05:14 -0500

    Merging POC into REx

--- Tests/Transit.Framework.Imaging.Tests/Program.cs
+++ Tests/Transit.Framework.Imaging.Tests/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using Transit.Addon.RoadExtensions.Highways.Highway2L;
 
 namespace Transit.Framework.Imaging.Tests
 {
@@ -22,65 +23,41 @@ namespace Transit.Framework.Imaging.Tests
 
         static void GenerationHW2L_Segment()
         {
-            var laneStart = 340;
-            var laneWidth = 141;
-            var lineAlpha = (byte)35;
-            var tearsAlpha = (byte)15;
-
-            var mainTex = ImageBlender
-                .FromBaseFile(@"Roads\Common\Textures\MainTex\Segment__MainTex.png")
-                .WithComponent(@"Roads\Common\Textures\MainTex\Line_White_Solid__MainTex.png", new Point(laneStart, 0))
-                .WithComponent(@"Roads\Common\Textures\MainTex\Line_White_Dashed__MainTex.png", new Point(laneWidth, 0))
-                .WithComponent(@"Roads\Common\Textures\MainTex\Line_White_Solid__MainTex.png", new Point(laneWidth, 0))
+            var mainTex = Highway2LBlenders
+                .GetSegmentMainTex()
                 .Apply();
 
-            var aprTex = ImageBlender
-                .FromBaseFile(@"Roads\Common\Textures\APRMap\Segment__APRMap.png")
-                .WithComponent(@"Roads\Common\Textures\APRMap\Line_Solid__APRMap.png", new Point(laneStart, 0), lineAlpha)
-                .WithComponent(@"Roads\Common\Textures\APRMap\Tearing__APRMap.png", new Point(1, 0), tearsAlpha)
-                .WithComponent(@"Roads\Common\Textures\APRMap\Line_Dashed__APRMap.png", new Point(0, 0), lineAlpha)
-                .WithComponent(@"Roads\Common\Textures\APRMap\Tearing__APRMap.png", new Point(0, 0), tearsAlpha)
-                .WithComponent(@"Roads\Common\Textures\APRMap\Line_Solid__APRMap.png", new Point(2, 0), lineAlpha)
+            var aprTex = Highway2LBlenders
+                .GetSegmentAPRMap()
                 .Apply();
+
+            mainTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Segment__MainTex.png", ImageFormat.Png);
+            aprTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Segment__APRMap.png", ImageFormat.Png);
 
             //var lookAndFeel = ImageBlender
             //    .FromImage(mainTex)
             //    .WithAlphaComponent(aprTex)
             //    .Apply();
-
-            mainTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Segment__MainTex.png", ImageFormat.Png);
-            aprTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Segment__APRMap.png", ImageFormat.Png);
             //lookAndFeel.Save("Segment__LAF.png", ImageFormat.Png);
         }
 
         static void GenerationHW2L_Node()
         {
-            var laneStart = 340;
-            var laneWidth = 141;
-            var lineAlpha = (byte)35;
-            var tearsAlpha = (byte)15;
-
-            var mainTex = ImageBlender
-                .FromBaseFile(@"Roads\Common\Textures\MainTex\Segment__MainTex.png")
-                .WithComponent(@"Roads\Common\Textures\MainTex\Line_White_Solid_Fadeout__MainTex.png", new Point(laneStart, 0))
-                .WithComponent(@"Roads\Common\Textures\MainTex\Line_White_Solid_Fadeout__MainTex.png", new Point(laneWidth + 4 + laneWidth, 0))
+            var mainTex = Highway2LBlenders
+                .GetNodeMainTex()
                 .Apply();
 
-            var aprTex = ImageBlender
-                .FromBaseFile(@"Roads\Common\Textures\APRMap\Segment__APRMap.png")
-                .WithComponent(@"Roads\Common\Textures\APRMap\Line_Solid_Fadeout__APRMap.png", new Point(laneStart, 0), lineAlpha)
-                .WithComponent(@"Roads\Common\Textures\APRMap\Tearing_Fadeout__APRMap.png", new Point(1, 0), tearsAlpha)
-                .WithComponent(@"Roads\Common\Textures\APRMap\Tearing_Fadeout__APRMap.png", new Point(4, 0), tearsAlpha)
-                .WithComponent(@"Roads\Common\Textures\APRMap\Line_Solid_Fadeout__APRMap.png", new Point(2, 0), lineAlpha)
+            var aprTex = Highway2LBlenders
+                .GetNodeAPRMap()
                 .Apply();
+
+            mainTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Node__MainTex.png", ImageFormat.Png);
+            aprTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Node__APRMap.png", ImageFormat.Png);
 
             //var lookAndFeel = ImageBlender
             //    .FromImage(mainTex)
             //    .WithAlphaComponent(aprTex)
             //    .Apply();
-
-            mainTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Node__MainTex.png", ImageFormat.Png);
-            aprTex.Save(@"D:\Developpement\Git\CSL.TransitAddonMod\Transit.Addon.RoadExtensions\Highways\Highway2L\Textures\Ground_Node__APRMap.png", ImageFormat.Png);
             //lookAndFeel.Save("Node__LAF.png", ImageFormat.Png);
         }
     }
