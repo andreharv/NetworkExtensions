4dddfd4da7e162ee66b98182bd712e238c209823
Katalyst6 <david_charlebois@msn.com>
2016-03-29 07:51:25 -0400

    WIP2

--- Transit.Addon.TM/PathFindingFeatures/TAMLaneRoutingManager.Directions.cs
+++ Transit.Addon.TM/PathFindingFeatures/TAMLaneRoutingManager.Directions.cs
@@ -1,9 +1,11 @@
 ﻿using ColossalFramework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Transit.Addon.TM.Data;
 using Transit.Addon.TM.Traffic;
 using Transit.Framework;
+using UnityEngine;
 
 namespace Transit.Addon.TM.PathFindingFeatures
 {
@@ -11,11 +13,74 @@ namespace Transit.Addon.TM.PathFindingFeatures
     {
         private TAMLaneRoute TransformToRoute(uint laneId, TAMLaneDirection direction)
         {
+            if (direction == TAMLaneDirection.None)
+            {
+                return null;
+            }
+
             var lane = NetManager.instance.m_lanes.m_buffer[laneId];
-            var segment = NetManager.instance.m_segments.m_buffer[lane.m_segment];
+            var segmentId = lane.m_segment;
+            var segment = NetManager.instance.m_segments.m_buffer[segmentId];
             var nodeId = NetManager.instance.FindLaneNode(laneId);
 
-            var similarLanes = segment.m_lanes
+            if (nodeId == null)
+            {
+                return null;
+            }
+
+            var segmentDir = segment.GetDirection(nodeId.Value);
+            var node = NetManager.instance.m_nodes.m_buffer[nodeId.Value];
+            var otherSegmentIds = node.GetSegmentIds().Except(segmentId).ToArray();
+            var connections = new List<uint>();
+
+            foreach (var otherSegmentId in otherSegmentIds)
+            {
+                var otherSegment = NetManager.instance.m_segments.m_buffer[segmentId];
+                var otherSegmentDir = otherSegment.GetDirection(nodeId.Value);
+
+                var relDirection = GetRelativeDirection(segmentDir, otherSegmentDir);
+
+                if ((relDirection & direction) != 0)
+                {
+                    // Good direction
+                    foreach (var item in otherSegment.GetL)
+                    {
+
+                    }
+                }
+            }
+
+            if (!connections.Any())
+            {
+                return null;
+            }
+
+            return new TAMLaneRoute()
+            {
+                LaneId = laneId,
+                NodeId = nodeId.Value,
+                Connections = connections.ToArray()
+            };
+        }
+
+        private TAMLaneDirection GetRelativeDirection(Vector3 source, Vector3 destination)
+        {
+            // TODO: Validate that
+            if (Vector3.Angle(source, destination) > 150f)
+            {
+                return TAMLaneDirection.Forward;
+            }
+            else
+            {
+                if (Vector3.Dot(Vector3.Cross(source, -destination), Vector3.up) > 0f)
+                {
+                    return TAMLaneDirection.Right;
+                }
+                else
+                {
+                    return TAMLaneDirection.Left;
+                }
+            }
         }
 
 
@@ -34,9 +99,9 @@ namespace Transit.Addon.TM.PathFindingFeatures
             if (arrows == null)
             {
                 // read currently defined arrows
-                uint laneFlags = (uint) Singleton<NetManager>.instance.m_lanes.m_buffer[laneId].m_flags;
+                uint laneFlags = (uint)Singleton<NetManager>.instance.m_lanes.m_buffer[laneId].m_flags;
                 laneFlags &= Flags.lfr; // filter arrows
-                arrows = (TAMLaneDirection) laneFlags;
+                arrows = (TAMLaneDirection)laneFlags;
             }
 
             arrows ^= flags;
@@ -63,15 +128,15 @@ namespace Transit.Addon.TM.PathFindingFeatures
                 return; // modification of arrows in highway rule mode is forbidden
 
             _laneDirections[laneId] = null;
-            uint laneFlags = (uint) Singleton<NetManager>.instance.m_lanes.m_buffer[laneId].m_flags;
+            uint laneFlags = (uint)Singleton<NetManager>.instance.m_lanes.m_buffer[laneId].m_flags;
 
-            if (((NetLane.Flags) laneFlags & NetLane.Flags.Created) == NetLane.Flags.None)
+            if (((NetLane.Flags)laneFlags & NetLane.Flags.Created) == NetLane.Flags.None)
             {
                 Singleton<NetManager>.instance.m_lanes.m_buffer[laneId].m_flags = 0;
             }
             else
             {
-                Singleton<NetManager>.instance.m_lanes.m_buffer[laneId].m_flags &= (ushort) ~Flags.lfr;
+                Singleton<NetManager>.instance.m_lanes.m_buffer[laneId].m_flags &= (ushort)~Flags.lfr;
             }
         }
 
