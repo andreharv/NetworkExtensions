c69699a2d6a1f671ea74f1aeac7cd3d6410cbde6
Katalyst6 <david_charlebois@msn.com>
2016-05-01 18:13:27 -0400

    Fix transition ui bug

--- Transit.Addon.RoadExtensions/Roads/PedestrianRoads/StoneSmall/ZonablePedestrianStoneSmallRoadBuilder.Texturing.cs
+++ Transit.Addon.RoadExtensions/Roads/PedestrianRoads/StoneSmall/ZonablePedestrianStoneSmallRoadBuilder.Texturing.cs
@@ -17,12 +17,13 @@ namespace Transit.Addon.RoadExtensions.Roads.PedestrianRoads.StoneSmall
                                 @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Segment__AlphaMap.png",
                                 @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Segment__XYSMap.png"),
                             new LODTextureSet
-                                (@"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Segment_LOD__MainTex.png",
+                               (@"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Segment_LOD__MainTex.png",
                                 @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Segment_LOD__AlphaMap.png",
                                 @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Segment_LOD__XYSMap.png"));
+
                         for (int i = 0; i < info.m_nodes.Length; i++)
                         {
-                            if (info.m_nodes[i].m_flagsRequired == NetNode.Flags.Transition)
+                            if (info.m_nodes[i].m_flagsRequired != NetNode.Flags.Transition)
                             {
                                 info.m_nodes[i].SetTextures(
                                     new TextureSet
@@ -30,7 +31,7 @@ namespace Transit.Addon.RoadExtensions.Roads.PedestrianRoads.StoneSmall
                                         @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Node__AlphaMap.png",
                                         @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Node__XYSMap.png"),
                                     new LODTextureSet
-                                        (@"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Node_LOD__MainTex.png",
+                                       (@"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Node_LOD__MainTex.png",
                                         @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Node_LOD__AlphaMap.png"));
                             }
                             else
@@ -40,7 +41,7 @@ namespace Transit.Addon.RoadExtensions.Roads.PedestrianRoads.StoneSmall
                                        (@"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Trans__MainTex.png",
                                         @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Trans__AlphaMap.png"),
                                     new LODTextureSet
-                                        (@"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Trans_LOD__MainTex.png",
+                                       (@"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Trans_LOD__MainTex.png",
                                         @"Roads\PedestrianRoads\StoneSmall\Textures\Ground_Trans_LOD__AlphaMap.png"));
                             }
                         }
@@ -50,35 +51,36 @@ namespace Transit.Addon.RoadExtensions.Roads.PedestrianRoads.StoneSmall
                 case NetInfoVersion.Elevated:
                 case NetInfoVersion.Bridge:
                     { 
-                    info.SetAllSegmentsTexture(
-                        new TextureSet
-                           (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment__MainTex.png",
-                            @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment__AlphaMap.png",
-                            @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment__XYSMap.png"),
-                        new LODTextureSet
-                            (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment_LOD__MainTex.png",
-                            @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment_LOD__AlphaMap.png",
-                            @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment_LOD__XYSMap.png"));
-                    for (int i = 0; i < info.m_nodes.Length; i++)
-                    {
-                        if (info.m_nodes[i].m_flagsRequired == NetNode.Flags.Transition)
-                        {
-                            info.m_nodes[i].SetTextures(
-                                new TextureSet
-                                   (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Node__MainTex.png",
-                                    @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Node__AlphaMap.png"));
-                        }
-                        else
-                        {
-                            info.m_nodes[i].SetTextures(
-                                new TextureSet
-                                   (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Trans__MainTex.png",
-                                    @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Trans__AlphaMap.png"));
-                        }
-                    }
+                        info.SetAllSegmentsTexture(
+                            new TextureSet
+                               (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment__MainTex.png",
+                                @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment__AlphaMap.png",
+                                @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment__XYSMap.png"),
+                            new LODTextureSet
+                                (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment_LOD__MainTex.png",
+                                @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment_LOD__AlphaMap.png",
+                                @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Segment_LOD__XYSMap.png"));
 
-                    break;
+                        for (int i = 0; i < info.m_nodes.Length; i++)
+                        {
+                            if (info.m_nodes[i].m_flagsRequired != NetNode.Flags.Transition)
+                            {
+                                info.m_nodes[i].SetTextures(
+                                    new TextureSet
+                                       (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Node__MainTex.png",
+                                        @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Node__AlphaMap.png"));
+                            }
+                            else
+                            {
+                                info.m_nodes[i].SetTextures(
+                                    new TextureSet
+                                       (@"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Trans__MainTex.png",
+                                        @"Roads\PedestrianRoads\StoneSmall\Textures\Elevated_Trans__AlphaMap.png"));
+                            }
                         }
+
+                        break;
+                    }
             }
         }
     }
