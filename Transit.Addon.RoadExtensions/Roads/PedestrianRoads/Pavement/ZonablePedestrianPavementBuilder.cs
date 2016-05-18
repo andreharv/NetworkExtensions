81be84a5f46aaadd3011ab09a0b42f185097f450
Katalyst6 <david_charlebois@msn.com>
2016-05-17 22:38:23 -0400

    Merge attempt

--- Transit.Addon.RoadExtensions/Roads/PedestrianRoads/Pavement/ZonablePedestrianPavementBuilder.cs
+++ Transit.Addon.RoadExtensions/Roads/PedestrianRoads/Pavement/ZonablePedestrianPavementBuilder.cs
@@ -11,7 +11,7 @@ namespace Transit.Addon.RoadExtensions.Roads.PedestrianRoads.Pavement
         public int UIOrder { get { return 20; } }
 
         public string Name { get { return NetInfos.New.ZONEABLE_PED_PAVEMENT; } }
-        public string DisplayName { get { return "[BETA] Zonable Pedestrian Concrete Road"; } }
+        public string DisplayName { get { return "[BETA] Zonable Pedestrian Paved Road"; } }
         public string Description { get { return "Paved roads are nicer to walk on than gravel."; } }
         public string ShortDescription { get { return "Parking, zoneable, restricted traffic [Traffic++ V2 required]"; } }
 
